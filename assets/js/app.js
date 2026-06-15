47683f99d0fe32efc42a31dca63bc9b7185b989b
kinefpck-gif <kinefpck@gmail.com>
2026-06-15 08:56:20 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -302,3 +302,28 @@ document.addEventListener("DOMContentLoaded", () => {
     updateStep();
 
 });
+// =====================================
+// PREMIUM ACCORDION
+// =====================================
+
+document
+.querySelectorAll(
+".accordion-header"
+)
+.forEach(button => {
+
+    button.addEventListener(
+        "click",
+        () => {
+
+            const item =
+                button.parentElement;
+
+            item.classList.toggle(
+                "active"
+            );
+
+        }
+    );
+
+});
